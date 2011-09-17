720c05b4ca23a18f67d0c482614d8e165cf1f7cf
Michał Górny <mgorny@gentoo.org>
2011-09-17 16:34:41 +0200

    Validate XML using embedded Schema rather than external DTD.

--- installmask/locationdb.py
+++ installmask/locationdb.py
@@ -4,14 +4,15 @@
 
 from collections import namedtuple
 
-import lxml.objectify
+import lxml.objectify, lxml.etree
 
 Location = namedtuple('Location', ('paths', 'description'))
 
 class LocationDB(object):
 	def __init__(self, path):
 		# XXX: make this lazy
-		p = lxml.objectify.makeparser(dtd_validation = True)
+		schema = lxml.etree.XMLSchema(lxml.etree.XML(ldb_schema))
+		p = lxml.objectify.makeparser(schema = schema)
 		t = lxml.objectify.parse(path, p)
 		self._root = t.getroot()
 
@@ -24,3 +25,26 @@ class LocationDB(object):
 					d = None
 				return Location(tuple(l.path), d)
 		raise KeyError('%s not found in LocationDB' % key)
+
+ldb_schema = '''
+
+<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
+	<xs:element name="install-mask">
+		<xs:complexType>
+			<xs:sequence>
+				<xs:element name="location" minOccurs="0" maxOccurs="unbounded">
+					<xs:complexType>
+						<xs:sequence>
+							<xs:element name="path" type="xs:string" maxOccurs="unbounded"/>
+							<xs:element name="description" type="xs:token" minOccurs="0"/>
+						</xs:sequence>
+
+						<xs:attribute name="id" type="xs:string" use="required"/>
+					</xs:complexType>
+				</xs:element>
+			</xs:sequence>
+		</xs:complexType>
+	</xs:element>
+</xs:schema>
+
+'''
